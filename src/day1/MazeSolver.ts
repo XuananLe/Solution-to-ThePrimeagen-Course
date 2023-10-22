6b5f775e895e58445662bd8120b0b45ca67042c9
xuananle <lexuanan18102004@gmail.com>
2023-10-22 17:24:13 +0700

    Add Sort

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,6 @@
+function canWalk(maze: string[], wall: string, point: Point): boolean {
+    
+}
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
 
 }
--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,23 @@
+function partition(arr : number[], low : number, high : number) : number  {
+    const pivot = arr[high];
+    let idx = low -1;
+    for(let i = low; i < high; i++) {
+        if(arr[i] < pivot) {
+            idx++;
+            [arr[idx], arr[i]] = [arr[i], arr[idx]];
+        }
+    }
+    idx++;
+    [arr[idx], arr[high]] = [arr[high], arr[idx]];
+    return idx;
+} // split the array and return the index of the pivot
+function qs(arr : number[], left : number, right : number) : void {
+    if(left < right) {
+        const pivot = partition(arr, left, right);
+        qs(arr, left, pivot - 1);
+        qs(arr, pivot + 1, right);
+    }
+}
 export default function quick_sort(arr: number[]): void {
-
+    qs(arr, 0, arr.length - 1);
 }
