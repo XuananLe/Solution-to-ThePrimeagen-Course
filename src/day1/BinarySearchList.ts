ccc80cfd95ec8b2fc81d4701872f19902af11f8e
xuananle <lexuanan18102004@gmail.com>
2023-10-22 15:01:43 +0700

    Add Search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let left : number = 0;
+    let right : number = haystack.length - 1;
+    while (left <= right) {
+        let mid : number = Math.floor((left + right) / 2);
+        if (haystack[mid] === needle) {
+            return true;
+        }
+        if (haystack[mid] < needle) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return false;
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,16 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    const jumpAmmount = Math.floor(Math.sqrt(breaks.length));  
+    let i = 0;
+    for(; i < breaks.length; i += jumpAmmount) {
+        if(breaks[i]) {
+            break;
+        }
+    }
+    i -= jumpAmmount;
+    for(let j = i; j < i + jumpAmmount; j++) {
+        if(breaks[j]) {
+            return j;
+        }
+    }
+    return -1;
 }
